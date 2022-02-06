f126079684c1e11b24f587aea7a3e651b26096cf
GabrielSalt <86675419+GabrielSalt@users.noreply.github.com>
2022-02-06 15:23:05 +0000

    Added emails to form

--- static/help.js
+++ static/help.js
@@ -1,5 +1,16 @@
+var params = new URLSearchParams(location.search)
+var choice = params.get('choice')
+if (choice == 'suggestion'){
+    subject = params.get('subject')
+    message = params.get('message')
+    email = ''
+}
+else {
+    subject = params.get('question')
+    message = ''
+    email = params.get('email')
+}
 
 
 
-
-window.location.replace("gabrielsalt.github.io");
+// window.location.replace("gabrielsalt.github.io");
